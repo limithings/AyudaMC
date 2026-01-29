616a1fc8bdda136fba9ea615ef106df29c12bae0
limithings <contactasb@gmail.com>
2026-01-29 18:15:09 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'ayudamc-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './styles.css',
+    './app.js',
+    './data.js',
+    './icon.png',
+    './icon-512.png',
+    './generador_claves.html',
+    './Flujo Descargos.pdf'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
